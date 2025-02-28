c6a69706db1765e4c47cd27eb5532dd0400191c1
leticiareginajf <leticia.reginajf@gmail.com>
2025-02-28 17:25:40 -0300

    node modules

---
+++ form/src/components/MyForm.js
@@ -0,0 +1,25 @@
+import "./MyForm.css";
+
+const MyForm = () => {
+  return (
+    <div>
+      {/* 1 - Criação de form */}
+
+      <form>
+        <div>
+          <label htmlFor="name">Nome: </label>
+          <input type="text" name="name" placeholder="Digite o seu nome" />
+        </div>
+        {/* 2 - Label envolvendo o input */}
+        <label>
+          <span>E-mail:</span>
+          <input type="email" name="email" placeholder="Digite o seu e-mail" />
+        </label>
+        <input type="submit" value="Enviar" />
+
+      </form>
+    </div>
+  );
+};
+
+export default MyForm;
