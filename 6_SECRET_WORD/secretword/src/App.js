89941c70709083c101644cfa62fb81459851f8e9
leticiareginajf <leticia.reginajf@gmail.com>
2025-03-04 09:08:48 -0300

    atualizacao

--- 6_SECRET_WORD/secretword/src/App.js
+++ 6_SECRET_WORD/secretword/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import StartScreen from './components/StartScreen';
+
 
 function App() {
   return (
     <div className="App">
-     <h1>SecretWord</h1>
+
+    <StartScreen />
     </div>
+
+    
   );
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-#teste
+# React - Projetos Realizados
+
+## Componentes
+## Projeto - Secret Word
